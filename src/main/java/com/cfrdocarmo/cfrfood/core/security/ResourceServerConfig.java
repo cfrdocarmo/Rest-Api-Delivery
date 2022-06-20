5984b339f0ef786173a1d02a43e2b6b20970ebce
cfrdocarmo <carlos.felipe.ramos67@aluno.ifce.edu.br>
2022-06-20 00:33:49 -0300

    Spring Security Enable and free CORS

---
+++ src/main/java/com/cfrdocarmo/cfrfood/core/security/ResourceServerConfig.java
@@ -0,0 +1,24 @@
+package com.cfrdocarmo.cfrfood.core.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic()
+                .and()
+                .authorizeRequests()
+                    .anyRequest().authenticated()
+                .and()
+                    .cors().and()
+                    .oauth2ResourceServer().opaqueToken();
+
+    }
+}
